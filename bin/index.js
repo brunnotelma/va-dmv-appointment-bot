7e513937efc49eade2d304871364e1059cf0ebf8
Brunno Telma <brunnotelma@gmail.com>
2021-06-01 01:31:25 -0400

    Add configuration for node-notifier

--- bin/index.js
+++ bin/index.js
@@ -4,6 +4,8 @@ const qs = require('qs');
 const cheerio = require('cheerio');
 const yargs = require("yargs");
 const axios = require("axios");
+const notifier = require('node-notifier');
+const path = require('path');
 
 const options = yargs
 .usage("Usage: -n <name>")
@@ -11,6 +13,14 @@ const options = yargs
 .option("d", { alias: "driverTest", describe: "Driver's License Test", type: "boolean" })
 .option("m", { alias: "motorcycleTest", describe: "Motorcycle Skills Test", type: "boolean" })
 .argv;
+
+const notifierConfig = {
+    title: 'DMV Appointment Bot',
+    message: 'Hey there!',
+    icon: path.join(__dirname, '../images/icon.png'),
+    sound: true,
+    wait: true
+};
 
 if (options.knowledgeTest) {
     lookupKnowledgeTestingDates(123);
@@ -23,7 +33,7 @@ async function lookupKnowledgeTestingDates(locationID) {
     console.log(`Searching for knowledge testing...`);
     const knowledgeTestPlaces = await axios.get("https://www.dmv.virginia.gov/onlineservices/learnersKnowledgeTesting.json").then(res => res.data);
 
-    console.log("locations", knowledgeTestPlaces);
+    // console.log("locations", knowledgeTestPlaces);
 
     // Request Dates
     axios.post("https://vadmvappointments.as.me/schedule.php?action=showCalendar&fulldate=1&owner=19444409&template=monthly", qs.stringify({
@@ -42,11 +52,19 @@ async function lookupKnowledgeTestingDates(locationID) {
     .then(res => res.data)
     .then(res => {
         const $ = cheerio.load(res);
-        const days = $(".activeday");
+        const daysHTMl = $(".activeday");
+        const days = []
 
-        days.each((i, calendarElement) => {
-            console.log(calendarElement.attribs.day)
+        daysHTMl.each((i, calendarElement) => {
+            days.push(new Date(calendarElement.attribs.day))
         });
+
+        notifier.notify({
+            ...notifierConfig,
+            message: "Found an appointment day!"
+        })
+        console.log(days[0])
+
     }).catch(err => {
         console.error(err)
     });
